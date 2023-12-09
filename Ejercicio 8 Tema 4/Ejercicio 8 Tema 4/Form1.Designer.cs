49b71c0127db8c9f3c56541ea0917cc56d28267a
Salvador P. <salvadorperezd@gmail.com>
2023-12-09 19:29:28 +0100

    Agregado el codigo

---
+++ Ejercicio 8 Tema 4/Ejercicio 8 Tema 4/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Ejercicio_8_Tema_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.validaMediaBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNota1 = new System.Windows.Forms.TextBox();
+            this.txtNota2 = new System.Windows.Forms.TextBox();
+            this.txtNota3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // validaMediaBtn
+            // 
+            this.validaMediaBtn.BackColor = System.Drawing.Color.YellowGreen;
+            this.validaMediaBtn.Location = new System.Drawing.Point(491, 103);
+            this.validaMediaBtn.Name = "validaMediaBtn";
+            this.validaMediaBtn.Size = new System.Drawing.Size(128, 81);
+            this.validaMediaBtn.TabIndex = 0;
+            this.validaMediaBtn.Text = "Validar y Calcular media";
+            this.validaMediaBtn.UseVisualStyleBackColor = false;
+            this.validaMediaBtn.Click += new System.EventHandler(this.validaMediaBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(185, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nota 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(185, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nota 2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(185, 202);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nota 3:";
+            // 
+            // txtNota1
+            // 
+            this.txtNota1.Location = new System.Drawing.Point(281, 54);
+            this.txtNota1.Name = "txtNota1";
+            this.txtNota1.Size = new System.Drawing.Size(107, 26);
+            this.txtNota1.TabIndex = 4;
+            // 
+            // txtNota2
+            // 
+            this.txtNota2.Location = new System.Drawing.Point(281, 126);
+            this.txtNota2.Name = "txtNota2";
+            this.txtNota2.Size = new System.Drawing.Size(107, 26);
+            this.txtNota2.TabIndex = 5;
+            // 
+            // txtNota3
+            // 
+            this.txtNota3.Location = new System.Drawing.Point(281, 199);
+            this.txtNota3.Name = "txtNota3";
+            this.txtNota3.Size = new System.Drawing.Size(107, 26);
+            this.txtNota3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Moccasin;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtNota3);
+            this.Controls.Add(this.txtNota2);
+            this.Controls.Add(this.txtNota1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.validaMediaBtn);
+            this.Name = "Form1";
+            this.Text = "Ejercicio 8 Tema 4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button validaMediaBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNota1;
+        private System.Windows.Forms.TextBox txtNota2;
+        private System.Windows.Forms.TextBox txtNota3;
+    }
+}
+
---
+++ Ejercicio 8 Tema 4/Ejercicio 8 Tema 4/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_8_Tema_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool validaNotas(int nota1, int nota2, int nota3)
+        {
+            bool valida = true;
+            if (nota1 < 0 || nota1 > 10)
+            {
+                valida = false;
+            }
+            if (nota2 < 0 || nota2 > 10)
+            {
+                valida = false;
+            }
+            if (nota3 < 0 || nota3 > 10)
+            {
+                valida = false;
+            }
+            return valida;
+        }
+        int calculaMedia(int nota1, int nota2, int nota3)
+        {
+            int notaMedia;
+
+            notaMedia = (nota1 + nota2 + nota3) / 3;
+
+            return notaMedia;
+        }
+        private void validaMediaBtn_Click(object sender, EventArgs e)
+        {
+            int nota1;
+            int nota2;
+            int nota3;
+            int notaMedia;
+            //bool valida;
+
+            nota1 = int.Parse(txtNota1.Text);
+            nota2 = int.Parse(txtNota2.Text);
+            nota3 = int.Parse(txtNota3.Text);
+
+            //valida = validaNotas(nota1, nota2, nota3);
+
+            if (validaNotas(nota1, nota2, nota3) == true)
+            {
+                notaMedia = calculaMedia(nota1, nota2, nota3);
+
+                MessageBox.Show("Las notas introducidas son correctas y su nota media es " + notaMedia);
+            }
+            else
+            {
+                MessageBox.Show("Las notas introducidas no son correctas.");
+            }
+        }
+    }
+}
